9c0db7b0610f464c4f66cad5c13bfbdf9d4adbf5
Ishraq Basher <145221138+ishraqb@users.noreply.github.com>
2025-02-26 13:55:53 -0500

    Delete src/tt_um_ishadder.v

--- src/tt_um_ishadder.v
+++
@@ -1,7 +0,0 @@
-module tt_um_ishadder (
-    input [7:0] ui_in,   // First 8-bit input
-    input [7:0] uio_in,  // Second 8-bit input
-    output [7:0] uo_out  // 8-bit output
-);
-    assign uo_out = ui_in + uio_in; // Perform 8-bit addition
-endmodule
